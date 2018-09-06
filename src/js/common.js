a51464153d6b2466b3fa032057bab16b4ca1dfed
transoman <vasiag10@gmail.com>
2018-09-06 13:23:17 +0300

    доверстана страница встреч

--- src/js/common.js
+++ src/js/common.js
@@ -85,16 +85,7 @@ $(window).on('load', function(){
             {
                 breakpoint: 768,
                 settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: false,
+                    arrows: true,
                     centerMode: true,
                     centerPadding: '40px',
                     slidesToShow: 1
